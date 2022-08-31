816885f6b328f690a746af159c80c0105c55e498
Conrado Lanusse <conrado.lanusse@gmail.com>
2022-08-30 21:23:01 -0300

    final

---
+++ Clase 16 - Final/app.js
@@ -0,0 +1,22 @@
+
+
+const numeros = [1, 2, 3]
+
+const numeros2 = numeros.map(n => n*2)
+
+console.log(numeros2)
+
+
+// console.log( document.querySelector('div') )
+// alert('INgrese su nombre')
+
+const user = {
+    nombre: 'Juan',
+    edad: 25
+}
+
+console.log(user)
+
+user.edad++
+
+console.log(user)
